c85cfb2b31f9db0458b08b12096cf11d8d334aa5
Muslima-coder <muslimayoqubova2008@gmail.com>
2025-10-02 15:04:36 +0500

    upd

--- components/ProductItem.tsx
+++ components/ProductItem.tsx
@@ -1,10 +1,11 @@
 import type { FC } from "react"
 import type { ProductType } from "../@types/ProductType"
+import Image from "next/image"
 
 const ProductItem:FC<{item:ProductType}> = ({item}) => {
   return (
     <div className="w-[350px] h-[500px] text-white bg-transparent shadow-xl shadow-slate-200 p-2 rounded-lg overflow-hidden ">
-            <img className="mb-2" src={item.images[0]} alt="Product img" width="350" height="200" />
+            <Image className="mb-2" src={item.images[0]} alt="Product img" width="350" height="200" />
             <div className="px-2 pb-2">
                 <h2 className="font-bold mb-[5px] text-[22px] ">{item.title}</h2>
                 <p className="font-medium line-clamp-2 mb-[5px]">{item.description}</p>
